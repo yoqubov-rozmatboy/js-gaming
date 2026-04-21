6a283e2d7a372864b7d60f3bc81d62232c96e570
rozmatboy <rozmatboyyakubov@gmail.com>
2026-04-21 14:27:17 +0300

    added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// Js codes
+// JS Codes
 
-const button = document.querySelector('.btn');
-const first_player = document.querySelector('.first-player');
-const second_player = document.querySelector('.second-player');
+const button = document.querySelector(".btn");
+const first_player = document.querySelector('.first_player')
+const second_player = document.querySelector('.second_player')
 
-button.addEventListener('click', ()=> {
-    const first_player_number = first_player.value;
-    const second_player_number = second_player.value;
-})
+button.addEventListener("click", () => {
+    const first_random_number = Math.round(Math.random() * 2);
+    const second_random_number = Math.round(Math.random() * 2);
+
+});
