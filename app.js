89c02d093cd6d5b3cd36309378bfe25ebf3d10a4
rozmatboy <rozmatboyyakubov@gmail.com>
2026-04-21 14:26:26 +0300

    added

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Js codes
+
+const button = document.querySelector('.btn');
+const first_player = document.querySelector('.first-player');
+const second_player = document.querySelector('.second-player');
+
+button.addEventListener('click', ()=> {
+    const first_player_number = first_player.value;
+    const second_player_number = second_player.value;
+})
